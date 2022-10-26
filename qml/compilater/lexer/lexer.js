e57517bd52186faa341bc4c1651f9bf9a4bf00f2
QWERN-9876r <dimadobriy111@gmail.com>
2022-10-26 22:17:29 +0400

    <save>

--- qml/compilater/lexer/lexer.js
+++ qml/compilater/lexer/lexer.js
@@ -19,11 +19,23 @@ function Lexer(dataArray) {
             })
              
             }
+            
+            if(data.split('<save>')[1]){if(!saveArray){var saveArray = new Array()}
+            const txt = String(data.split('<save>')[1]).split('</save>')[0];
+            saveArray.push({name: txt.split(':')[0].replace(/\n/g,'').replace(/\s/,''),
+            script: String(txt.split('{')[1]).split('}')[0]}) }
+            
+            if(saveArray){
+                saveArray.map(save => {
+                    data.replace(`<${save.name}>`, save.script)
+                }) 
+            }
             if(data.split('<s>')[1]){lexem.script = String(data.split('<s>')[1]).split('</s>')[0]}
             if(data.split('<m>')[1]){lexem.html = String(data.split('<m>')[1]).split('</m>')[0]}
             if(data.split('<st>')[1]){lexem.style = String(data.split('<st>')[1]).split('</st>')[0]}
             if(data.split('<settings>')[1]){lexem.settings = String(data.split('<settings>')[1]).split('</settings>')[0]}
             if(data.split('<name>')[1]){lexem.namePage = String(data.split('<name>')[1]).split('</name>')[0]}
+            
             return lexem
     }
     var lexemArray = new Array();
@@ -44,13 +56,24 @@ function Lexer(dataArray) {
         })
         
         }
-        if(data.split('<s>')[1]){lexem.script = String(data.split('<s>')[1]).split('</s>')[0]}
-        if(data.split('<m>')[1]){lexem.html = String(data.split('<m>')[1]).split('</m>')[0]}
-        if(data.split('<st>')[1]){lexem.style = String(data.split('<st>')[1]).split('</st>')[0]}
-        if(data.split('<settings>')[1]){lexem.settings = String(data.split('<settings>')[1]).split('</settings>')[0]}
-        if(data.split('<name>')[1]){lexem.namePage = String(data.split('<name>')[1]).split('</name>')[0]}
+        if(data.split('<save>')[1]){if(!saveArray){var saveArray = new Array()}
+            const txt = String(data.split('<save>')[1]).split('</save>')[0];
+            saveArray.push({name: txt.split(':')[0].replace(/\n/g,'').replace(/\s/,''),
+            script: String(txt.split('{')[1]).split('}')[0]}) }
+            
+            if(saveArray){
+                saveArray.map(save => {
+                    data = data.replaceAll(`<${save.name}>`, save.script)
+                    
+                }) 
+            }
+            if(data.split('<s>')[1]){lexem.script = String(data.split('<s>')[1]).split('</s>')[0]}
+            if(data.split('<m>')[1]){lexem.html = String(data.split('<m>')[1]).split('</m>')[0]}
+            if(data.split('<st>')[1]){lexem.style = String(data.split('<st>')[1]).split('</st>')[0]}
+            if(data.split('<settings>')[1]){lexem.settings = String(data.split('<settings>')[1]).split('</settings>')[0]}
+            if(data.split('<name>')[1]){lexem.namePage = String(data.split('<name>')[1]).split('</name>')[0]}
         lexemArray.push(lexem)
-        if(lexemArray.length === dataArray.length){;Parser(lexemArray)}
+        if(lexemArray.length === dataArray.length){;Parser(lexemArray, )}
     
     })
 }
