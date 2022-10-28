f228d92ef1d8c58b3fb8c0df2695952dadad2b4d
QWERN-9876r <dimadobriy111@gmail.com>
2022-10-28 14:16:25 +0400

    SingleScript started

--- qml/compilater/index.js
+++ qml/compilater/index.js
@@ -2,7 +2,7 @@ const fs = require('fs'),
  Lexer = require('./lexer/lexer').Lexer,
  Parser = require('./parser/parser').Parser;
 
-fs.readFile('../test/index.tml', 'utf-8', function(err, data){
+fs.readFile('../test/index.sin', 'utf-8', function(err, data){
     
     
     if(err){
@@ -13,7 +13,7 @@ fs.readFile('../test/index.tml', 'utf-8', function(err, data){
         var DataArray
         Lexer(data).import.map(function (importObj) {
             this.DA = new Array();
-            fs.readFile(`../${importObj.path}.tml`, 'utf-8',(err, dataImport) => {
+            fs.readFile(`../${importObj.path}.sin`, 'utf-8',(err, dataImport) => {
                 
             if(!this.DA.length){this.DA.push(data);}
                 if(err){throw err}
@@ -32,7 +32,7 @@ fs.readFile('../test/index.tml', 'utf-8', function(err, data){
     } else {
         var DataArray = new Array();
     DataArray.push(data)
-    fs.writeFile('tml.js', Parser(Lexer(DataArray)), function(err){
+    fs.writeFile('singleScript.js', Parser(Lexer(DataArray)), function(err){
         if(err) {
             throw err
         }
--- qml/compilater/parser/parser.js
+++ qml/compilater/parser/parser.js
@@ -37,7 +37,7 @@ function Parser(lexemArray) {
      } else {pageName = 'main'}
     // lexemArray.map(lexem => {lexem.import.map(im => {if(im.name === 'main'){throw new Error('the page cannot be called "main"')}})})
     var index = lexemArray[0].import.length - 1;
-    fs.writeFile('tml.js', `
+    fs.writeFile('singleScript.js', `
     function CreateStyle(el,style){el.style.width=style.width;el.style.height=style.height;el.style.background=style.background;el.style.border=style.border;el.style.borderRadius=style.borderRadius;if(style.fontFamily){el.style.fontFamily=style.fontFamily}el.style.color=style.color;el.style.backgroundColor=style.backgroundColor;el.style.position=style.position;el.style.top=style.top;el.style.left=style.left;el.style.right=style.right;el.style.bottom=style.bottom;el.style.padding=style.padding;el.style.fontSize=style.fontSize;el.style.paddingLeft=style.paddingLeft;el.style.paddingRight=style.paddingRight;el.style.paddingTop=style.paddingTop;el.style.paddingBottom=style.paddingBottom;el.style.margin=style.margin;el.style.marginLeft=style.marginLeft;el.style.marginRight=style.marginRight;el.style.marginTop=style.marginTop;el.style.marginBottom=style.marginBottom};function Upgrade(el,changes){if(typeof el==='string'){try{el=document.querySelector(el)}catch{throw new Error('The specified element is missing')}};if(changes.style){CreateStyle(el,changes.style)};if(changes.txt){el.textContent=changes.txt};if(changes.onclick){el.onclick=changes.onclick};if(changes.placeHolder){el.placeHolder=changes.placeHolder}};
     ${JSA.map(function(jst){ 
         if(jst === JSA[0]){
